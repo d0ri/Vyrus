807fe902483d045192cc0327b955079bdb13f189
d0ri <alexander.freisinger@gmail.com>
2016-04-29 12:34:09 +0200

    Skript für zufällige Gegnerbewegung schreiben
    Skript geschrieben und drei Dummies eingefügt; Bugfixes für
    Antikörperbewegung

--- Vyrus_Unity/Assets/Scripts/Antikörperbewegung.cs
+++ Vyrus_Unity/Assets/Scripts/Antikörperbewegung.cs
@@ -6,21 +6,25 @@ public class Antikörperbewegung : MonoBehaviour {
 	GameObject Virus; //Virus
 	Vector3 direction; //Vektor von Antikörper zu Virus
 	float speed; //Variable Geschwindigkeit
+	public float baseSpeed = 1f; //Grundgeschwindigkeit
 	public float speedfactor = .1f; //Multiplikator Geschwindigkeit
 	Color virCol; //Farbe Virus
 	Color antiCol; //Farbe Antikörper
 	float r; //Farbunterschied roter Kanal
 	float g; //--grüner Kanal
 	float b; //--blauer Kanal
-	float rgbo; //--RGB unterschied (-1.5, 0, 1.5)
+	float rgbo; //--RGB unterschied (0, 3)
 	Vector3 erratic; //Zufallskomponente der Bewegung
+	float timer = 0f; //Timer
+	public float duration = 5f; //Dauer bis Richtungsänderung
 
 	void Start () {
 		Virus = GameObject.FindGameObjectWithTag ("Player"); //findet Virus mit Tag
+		timer = duration; // für Vektorberechnung bei Start
 
-		//ZUFALLSFARBE
+		//ZUFALLSFARBE!
 		GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
-		//TEST
+		//TEST!
 	}
 
 
@@ -32,20 +36,21 @@ public class Antikörperbewegung : MonoBehaviour {
 		r = Mathf.Abs(virCol.r - antiCol.r); //berechnet Farbunterschied roter Kanal
 		g = Mathf.Abs(virCol.g - antiCol.g); //--grüner Kanal
 		b = Mathf.Abs(virCol.b - antiCol.b); //--blauer kanal
-		rgbo = 1.5f - (r+g+b); //--RGB unterschied (-1.5, 0, 1.5)
-		speed = speedfactor * rgbo; //Geschwindigkeit berechnen
+		rgbo = 3f - (r+g+b); //--RGB unterschied (0, 3)
+		speed = baseSpeed + speedfactor * rgbo; //Geschwindigkeit berechnen
 
-		if (rgbo <= 0){
-			GetComponent<Rigidbody>().AddForce(erratic.normalized, ForceMode.VelocityChange); //für großen Unterschied
+		if (timer <= duration) {
+			timer += Time.deltaTime; //zählt
+		} else {
+			erratic = new Vector3 (Random.Range(-1f,1f), 0, Random.Range(-1,1f)); //berechnet Zufallsvektor 
+			timer = 0; //setzt Timer zurück
+		}
+
+		if (rgbo <= 2f){
+			GetComponent<Rigidbody>().AddForce(erratic.normalized*baseSpeed, ForceMode.VelocityChange); //für großen Unterschied
 		}
 			else {
 			GetComponent<Rigidbody>().AddForce(direction.normalized*speed, ForceMode.VelocityChange); //für kleinen Unterschied
 			}
-		StartCoroutine(errChange()); // startet Coroutine
-	}
-
-	IEnumerator errChange(){
-		erratic = new Vector3 (Random.Range(-1f,1f), 0, Random.Range(-1,1f)); //berechnet neuen Zufallsvektor
-		yield return new WaitForSeconds(5f); //wartet 5 Sekunden
 	}
 }
