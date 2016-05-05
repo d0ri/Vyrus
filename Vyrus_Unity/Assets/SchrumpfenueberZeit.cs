4afbd692874d6309d2d8aa03dc679773c7eac79a
d0ri <alexander.freisinger@gmail.com>
2016-05-05 22:08:49 +0200

    Groesserwerden implementiert

---
+++ Vyrus_Unity/Assets/SchrumpfenueberZeit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SchrumpfenueberZeit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Decrease", 0, .1f);
+	}
+
+	void Decrease () {   //Größe des Spielers nimmt ab bis 1.0
+		if (transform.localScale.x >= 1f) {
+			transform.localScale *= .998f;
+		}
+	}
+
+}
---
+++ Vyrus_Unity/Assets/Zellen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zellen : MonoBehaviour {
+
+
+Vector3 posstart; //Position am Anfang des Fressvorgangs
+public float zeitseitstart = 0f; //Zeit seit Beginn des Fressvorgangs
+	void Update(){ //Leitet Fressvorgang ein
+		if (transform.parent.tag == "Player") {
+			StartCoroutine (Zerstören());
+		}
+	}
+IEnumerator Zerstören() {
+	zeitseitstart += Time.deltaTime; //Beginnt die Zeit zu zählen
+	posstart = transform.position; //Legt die Ausgangsposition fest
+	transform.position = Vector3.Lerp (posstart,transform.parent.transform.position, zeitseitstart/100f); //Bewegt die Zelle zur Position des Virus
+	yield return new WaitForSeconds(5f); //			
+	Destroy(this.gameObject); //Zerstört Zelle
+	zeitseitstart = 0f; //Setzt Timer zurück
+	}
+}
