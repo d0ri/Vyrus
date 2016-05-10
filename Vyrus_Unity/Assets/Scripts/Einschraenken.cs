a872f7a79a192ee17ef097f6a34a35f0da214b9f
d0ri <alexander.freisinger@gmail.com>
2016-05-11 01:26:23 +0200

    einschränken durch einschraenken ersetzt
    um konflikte zu vermeiden

--- Vyrus_Unity/Assets/Scripts/Einschraenken.cs
+++ Vyrus_Unity/Assets/Scripts/Einschraenken.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Einschränken : MonoBehaviour {
+public class Einschraenken : MonoBehaviour {
 
 	public GameObject virus;
 	public float langsamkeit = 1f;
