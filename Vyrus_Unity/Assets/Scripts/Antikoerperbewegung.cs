c63744e2d7cc419e651dd2e91568f00ece771e1c
d0ri <alexander.freisinger@gmail.com>
2016-05-14 23:09:35 +0200

    Antikoerperbewegung verbessert
    Bewegung von AddForce zu Vector3.Lerp umgeaendert.

--- Vyrus_Unity/Assets/Scripts/Antikoerperbewegung.cs
+++ Vyrus_Unity/Assets/Scripts/Antikoerperbewegung.cs
@@ -14,7 +14,7 @@ public class Antikoerperbewegung : MonoBehaviour {
 	float g; //--grüner Kanal
 	float b; //--blauer Kanal
 	public float rgbo; //--RGB unterschied (0, 3)
-	Vector3 erratic; //Zufallskomponente der Bewegung
+//	Vector3 erratic; //Zufallskomponente der Bewegung
 	float timer = 0f; //Timer
 	public float duration = 5f; //Dauer bis Richtungsänderung
 
@@ -33,51 +33,64 @@ public class Antikoerperbewegung : MonoBehaviour {
 	}
 
 
+	void OnTriggerEnter(Collider other){
+		if (other.transform.tag == "Hindernis") {
+			transform.rotation = Quaternion.Euler (0, 180f, 0);
+		}
+	}
+
 	void Update () {
 		direction = (Virus.transform.position - transform.position); //berechnet Vektor von Antikörper zu Virus
-		orientierung = Quaternion.LookRotation(Virus.transform.position-transform.position)*Quaternion.Euler(0,drehung,0); //der antikoerper orientiert sich richtung virus
-		transform.rotation = Quaternion.Lerp(transform.rotation,orientierung,.5f);
+
 		virCol = Virus.GetComponent<Renderer>().material.GetColor("_SpecColor"); //Definition virMat
 		antiCol = GetComponent<Renderer>().material.color; //Definition antiMat
 		r = Mathf.Abs(virCol.r - antiCol.r); //berechnet Farbunterschied roter Kanal
 		g = Mathf.Abs(virCol.g - antiCol.g); //--grüner Kanal
 		b = Mathf.Abs(virCol.b - antiCol.b); //--blauer kanal
-		rgbo = 3f - (r+g+b); //--RGB unterschied (0, 3)
-		speed = baseSpeed + speedfactor * rgbo; //Geschwindigkeit berechnen
+		rgbo = 2.5f*(3f - (r+g+b)); //--RGB unterschied (0, 3)
+		//speed = baseSpeed + speedfactor * rgbo; //Geschwindigkeit berechnen
+
 
 		if (timer <= duration) {
 			timer += Time.deltaTime; //zählt
 		} else {
-			erratic = new Vector3 (Random.Range(-1f,1f), 0, Random.Range(-1,1f)); //berechnet Zufallsvektor 
+			if(direction.magnitude >= 20f){; //berechnet Zufallsvektor
+				transform.rotation = Quaternion.Euler(0,Random.value*45f,0);
 			timer = 0; //setzt Timer zurück
+			}
 		}
 
 		if (rgbo <= 2f || direction.magnitude >= 20f ){
+
 			if(vorwaerts == 1){
-				transform.position += transform.forward * Time.deltaTime * 10; 
+				transform.position += transform.forward * Time.deltaTime * 1.5f; 
 			}
 			if(vorwaerts == 2){
-				transform.position += transform.right * Time.deltaTime * 10; 
+				transform.position += transform.right * Time.deltaTime * 1.5f; 
 			}
 			if(vorwaerts == 3){
-				transform.position += transform.forward * Time.deltaTime * -1f * 10; 
+				transform.position += transform.forward * Time.deltaTime * -1f * 1.5f; 
 			}
 			if(vorwaerts == 4){
-				transform.position += transform.right * Time.deltaTime * -1f * 10; 
+				transform.position += transform.right * Time.deltaTime * -1f * 1.5f; 
 			}
 		}
 			else {
+
+			orientierung = Quaternion.LookRotation(Virus.transform.position-transform.position)*Quaternion.Euler(0,drehung,0); //der antikoerper orientiert sich richtung virus
+			transform.rotation = Quaternion.Lerp(transform.rotation,orientierung,.5f); //Lerp
+
 			if(vorwaerts == 1){
-			transform.position += transform.forward * Time.deltaTime * 10; 
+				transform.position += transform.forward * Time.deltaTime * rgbo; 
 			}
 			if(vorwaerts == 2){
-				transform.position += transform.right * Time.deltaTime * 10;
+				transform.position += transform.right * Time.deltaTime * rgbo;
 			}
 			if(vorwaerts == 3){
-				transform.position += transform.forward * Time.deltaTime * -1f * 10;
+				transform.position += transform.forward * Time.deltaTime * -1f * rgbo;
 			}
 			if(vorwaerts == 4){
-				transform.position += transform.right * Time.deltaTime * -1f * 10; 
+				transform.position += transform.right * Time.deltaTime * -1f * rgbo; 
 			}
 			}
 	}
