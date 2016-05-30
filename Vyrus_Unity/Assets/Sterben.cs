fdb9530123e72fbc60b43442762de72cb92dbcd7
d0ri <alexander.freisinger@gmail.com>
2016-05-30 23:59:52 +0200

    Sterben

---
+++ Vyrus_Unity/Assets/Sterben.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Sterben : MonoBehaviour {
+
+	public GameObject TexturePlane; // Plane mit Videotextur "Game Over"
+	public GameObject Musik;
+	public AudioClip Sterbesound;
+	public ParticleSystem partsys;
+	public MovieTexture got;
+
+	void Update () {
+		if (transform.localScale.x <= 40.0f){
+			StartCoroutine (Stirb ());
+		}
+	}
+	IEnumerator Stirb() {
+		Musik.SetActive (false);
+		GetComponent<Renderer> ().enabled = false;
+		partsys.gameObject.SetActive (true);
+		AudioSource.PlayClipAtPoint (Sterbesound,transform.position);
+		yield return new WaitForSeconds(2f);
+		TexturePlane.SetActive (true);
+		//got = TexturePlane.GetComponent<Renderer> ().material.mainTexture;
+		got.Play ();
+
+		yield return new WaitForSeconds(3f); //
+		TexturePlane.SetActive (false);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
