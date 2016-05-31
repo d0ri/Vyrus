f2ca794c791b7ada09a887c914fb09273d5cdfa0
Susan Martin <susi11189@aol.com>
2016-05-31 20:56:05 +0200

    Magnet Scripte erstellt

---
+++ Vyrus_Unity/Assets/Scripts/MagnetVirus.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetVirus : MonoBehaviour {
+
+	public bool nag = false; // magnetisch ja/nein
+	float countdown = 10.0f;
+
+
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == "Magnet") {
+			nag = true;
+			Destroy (other);}
+
+		if (nag = true) {
+			countdown -= Time.deltaTime;}
+
+		if (countdown <= 0) { 
+			nag = false;
+			countdown = 10.0f;}
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Vyrus_Unity/Assets/Scripts/MagnetZelle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetZelle : MonoBehaviour {
+
+	public GameObject Virus; 
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Virus.GetComponent<MagnetVirus> ().nag == true) {
+
+			transform.position = Vector3.Lerp(transform.position,Virus.transform.position,Time.deltaTime*0.5f);}
+
+
+	}
+}
