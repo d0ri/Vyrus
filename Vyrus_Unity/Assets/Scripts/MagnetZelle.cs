74fd6b5e5936478e5e68e78fe38d87b532e0315f
Susan Martin <susi11189@aol.com>
2016-06-01 14:58:08 +0200

    MagnetZelle Script fertig

--- Vyrus_Unity/Assets/Scripts/MagnetZelle.cs
+++ Vyrus_Unity/Assets/Scripts/MagnetZelle.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MagnetZelle : MonoBehaviour {
 
-	public GameObject Virus; 
+    GameObject Virus;
 
+    void Start()
+    {
+        Virus = GameObject.FindGameObjectWithTag("Player");
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-	// Update is called once per frame
-	void Update () {
+        if (Virus.GetComponent<MagnetVirus>().nag == true)
 
-		if (Virus.GetComponent<MagnetVirus> ().nag == true) {
+        { if (Distance(transform.position-Virus.transform.position) <= 10.0f)
 
-			transform.position = Vector3.Lerp(transform.position,Virus.transform.position,Time.deltaTime*0.5f);}
+            { transform.position = Vector3.Lerp(transform.position, Virus.transform.position, Time.deltaTime * 0.5f); }
+        }
+    }
 
-
-	}
+    private float Distance(Vector3 vector3)
+    {
+        throw new NotImplementedException();
+    }
 }
