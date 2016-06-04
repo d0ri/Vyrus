c3596634c30cf033d2b286474456289d0201aacf
Johanna Lauterbach <Johanna.lauterbach@gmx.de>
2016-06-04 13:06:05 +0200

    pause als ui + skript

--- Vyrus_Unity/Assets/pause.cs
+++ Vyrus_Unity/Assets/pause.cs
@@ -5,13 +5,25 @@ public class pause : MonoBehaviour {
 
     public GameObject pauseButton, pausePanel;
 
-    // Use this for initialization
-    void Start () {
-	
+	public void Start()
+	{
+		OnUnpause();
+	}	
+
+	public void OnPause()
+	{
+		pausePanel.SetActive(true);
+		pauseButton.SetActive(false);
+		Time.timeScale = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void OnUnpause()
+	{
+		pausePanel.SetActive(false);
+		pauseButton.SetActive(true);
+		Time.timeScale = 1;
 	}
 }
+
+
+
