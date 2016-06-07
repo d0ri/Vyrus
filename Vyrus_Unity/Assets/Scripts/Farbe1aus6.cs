8875b0b2ce92a0c297436bcb9abd47b4593a5a25
Carolin Ortmann <ortmann.caroline@icloud.com>
2016-06-08 00:23:45 +0200

    Skript zufällige Farben

--- Vyrus_Unity/Assets/Scripts/Farbe1aus6.cs
+++ Vyrus_Unity/Assets/Scripts/Farbe1aus6.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class Farbe1aus6 : MonoBehaviour {
+
+	public int zahl;
 		public Color[] farben = new Color[5]; //Liste von 5 verschiedenen Farben
 	void Start () {
-		GetComponent<Renderer> ().material.SetColor ("_SpecColor", farben [(Random.Range (0, 5))]); //Zufällige Auswahl aus den 6 Farben
+		GetComponent<Renderer> ().material.SetColor ("_SpecColor", farben [(Random.Range (0, zahl))]); //Zufällige Auswahl aus den 6 Farben
 	}
 }
