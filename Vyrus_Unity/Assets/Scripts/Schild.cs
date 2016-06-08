53e1b339935993fa2a42ac81c8349cae51894e63
Anuschka Schneyer <a.s1996@web.de>
2016-06-08 11:47:26 +0200

    SchildSkript

---
+++ Vyrus_Unity/Assets/Scripts/Schild.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Schild : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.transform.tag == "Schild") {
+			Destroy (other.gameObject);
+			GameObject.FindGameObjectWithTag ("Schutz").SetActive (true);
+		}
+		}
+		
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
