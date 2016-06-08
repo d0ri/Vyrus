4bd4181eb19e4b43c68951adee7094756860af28
Johanna Lauterbach <Johanna.lauterbach@gmx.de>
2016-06-08 21:55:13 +0200

    1.versuch neustarten

---
+++ Vyrus_Unity/Assets/neustarten.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class neustarten : MonoBehaviour {
+	
+	public GameObject neustartenButton, pausePanel;
+
+	public void restartCurrentScene() {
+		
+	}
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
