e977d31fb23e4fdafd85c3b1fdba8146b6b0ddb6
Alex <alexander.freisinger@gmail.com>
2016-06-18 01:50:53 +0200

    Organe infizieren verbessert
    nötige manuelle Einstellung auf Minimum reduziert + UI an aktuelle
    Unity-Version angepasst

--- Vyrus_Unity/Assets/Scripts/SpawnZellen.cs
+++ Vyrus_Unity/Assets/Scripts/SpawnZellen.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class SpawnZellen : MonoBehaviour {
 
 	public GameObject Zelle;
+	GameObject neueZelle;
 	public int maxZellen = 100;
 	public float spread = 3;
 
 	void Update (){
-		if(GameObject.FindGameObjectsWithTag("Zelle").Length<maxZellen){
-			Instantiate (Zelle, transform.position + Vector3.up * 0.2f + new Vector3 (spread * Random.Range (-1f, 1f), 0, spread * Random.Range (-1f, 1f)), Quaternion.identity);
+		if(transform.childCount<maxZellen){
+			GameObject neueZelle = Instantiate (Zelle, transform.position + Vector3.up * 0.2f + new Vector3 (spread * Random.Range (-1f, 1f), 0, spread * Random.Range (-1f, 1f)), Quaternion.identity) as GameObject;
+			neueZelle.transform.parent = this.transform;
 		}
 	}
 }
