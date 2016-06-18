16d9fb8752be2499341544a2d40db99566b3d545
Alex <alexander.freisinger@gmail.com>
2016-06-18 02:18:59 +0200

    Revert "Organe infizieren verbessert"
    This reverts commit e977d31fb23e4fdafd85c3b1fdba8146b6b0ddb6.

---
+++ Vyrus_Unity/Assets/Scripts/neustarten.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class restart : MonoBehaviour {
+	
+	public Canvas pausePanel;
+	public Button restart;
+
+	public void restartCurrentScene() {
+		
+	}
+	void Start () {
+
+		pausePanel = pausePanel.GetComponent<Canvas> ();
+		restart = restart.GetComponent<Button> ();
+	
+	}
+
+	public void ExitPress()
+	{
+	pausePanel.enabled = true;
+	restart.enabled = false;
+	}
+
+	public void NoPress()
+	{
+		pausePanel.enabled = false;
+		restart.enabled = true;
+	}
+
+	public void StartLevel()
+	{
+		Application.loadedLevel();
+	}
+
+}
---
+++ Vyrus_Unity/Assets/Scripts/pause.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+
+    public GameObject pauseButton, pausePanel;
+
+	public void Start()
+	{
+		OnUnpause();
+	}	
+
+	public void OnPause()
+	{
+		pausePanel.SetActive(true);
+		pauseButton.SetActive(false);
+		Time.timeScale = 0;
+	}
+
+	public void OnUnpause()
+	{
+		pausePanel.SetActive(false);
+		pauseButton.SetActive(true);
+		Time.timeScale = 1;
+	}
+}
+
+
+
