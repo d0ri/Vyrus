4f2b339b0961917d99e755a72939eef4216f590c
Anuschka Schneyer <a.s1996@web.de>
2016-06-22 13:15:15 +0200

    Skript Infotafel erstellt

---
+++ Vyrus_Unity/Assets/Scripts/startscreenscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class startscreenscript : MonoBehaviour {
+	public Canvas can;
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Time.timeScale = 1;
+			can.enabled = false;
+		}
+	}
+}
