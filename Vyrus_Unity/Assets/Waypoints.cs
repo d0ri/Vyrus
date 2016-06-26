824cf79b1714e44161b4bd8922439c1707a9f3e6
Alex <alexander.freisinger@gmail.com>
2016-06-26 23:35:52 +0200

    Zwischencommit Waypointscript

---
+++ Vyrus_Unity/Assets/Waypoints.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+
+	public Transform[] path;
+	public float speed = 5.0f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+
+	void Update () {
+		//Vector3 dir = path [currentPoint].position - transform.position;
+		float dist = Vector3.Distance(path[currentPoint].position,transform.position);
+		transform.position = Vector3.MoveTowards(transform.position,path[currentPoint].position, Time.deltaTime*speed);
+		if (dist <= reachDist) {
+			currentPoint ++;
+		}
+		if (currentPoint >= path.Length) {
+			currentPoint = 0;
+		}
+	}
+	void OnDrawGizmos(){
+		if (path.Length > 0)
+			for (int i = 0; i < path.Length; i++) {
+				if (path [i] != null) {
+					Gizmos.DrawSphere (path [i].position, reachDist);
+				}
+			}
+	}
+}
