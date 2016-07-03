ba1dca0747bba18f90bae1d16a4d0b6ce61969b6
Alex <alexander.freisinger@gmail.com>
2016-07-03 21:12:45 +0200

    bugfixes und Optimierung

--- Vyrus_Unity/Assets/Scripts/Antikoerperkollision.cs
+++ Vyrus_Unity/Assets/Scripts/Antikoerperkollision.cs
@@ -2,25 +2,20 @@
 using System.Collections;
 
 public class Antikoerperkollision : MonoBehaviour {
-	//public AudioClip Fressgeräusch;
-	public AudioClip AnitkörperTrifftSound;
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	void OnTriggerEnter(Collider other){
-
-		if (other.transform.tag == "Anti") {
-			transform.localScale = (transform.localScale -= new Vector3 (.1f, .1f, .1f)) * 0.9f;
-			Destroy (other.gameObject);
-			AudioSource.PlayClipAtPoint (AnitkörperTrifftSound, transform.position);
-		}
-		}
-
-	// Update is called once per frame
-		void Update () {
-	
+	public AudioClip Fressgeräusch;
+	public AudioClip AntikörperTrifftSound;
 		
+	void OnTriggerEnter(Collider other){
+		if (other.transform.tag == "Anti") {
+			Destroy (other.gameObject);
+			if (GetComponent<Makrophagenskript> ().mak == false) {
+				transform.localScale = (transform.localScale -= new Vector3 (.1f, .1f, .1f)) * 0.9f;
+				AudioSource.PlayClipAtPoint (AntikörperTrifftSound, transform.position);
+			}
+			if (GetComponent<Makrophagenskript> ().mak == true) {
+				transform.localScale += new Vector3 (20f, 20f, 20f);
+				AudioSource.PlayClipAtPoint (Fressgeräusch, transform.position);
+			}
+		}
 	}
 }
