aad1003bb6500084ebbfea4c83f785e7705c6c92
Carolin Ortmann <ortmann.caroline@icloud.com>
2016-07-03 18:20:44 +0200

    Skript Makrophase und Level 3 angelegt

---
+++ Vyrus_Unity/Assets/Scripts/Makrophagenskript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Makrophagenskript : MonoBehaviour {
+
+		public AudioClip Fressgeräsch;
+		float anzahl = 0f; // Anzahl gefressener Zellen
+		public bool mak = false; // magnetisch ja/nein
+		float countdown = 10.0f;
+		GameObject makrophage;
+
+
+		void OnTriggerEnter(Collider other){
+
+			if (other.transform.tag == "Makrophage") {
+				other.transform.parent = this.transform;
+				other.GetComponent<Collider> ().enabled = false;
+				AudioSource.PlayClipAtPoint (Fressgeräsch, transform.position,100);
+					mak = true;
+					makrophage = other.gameObject;
+					makrophage.SetActive (false);
+			        mak = false;
+					//Destroy (other.gameObject);}
+
+				
+
+				
+					if (mak == true) {	
+						countdown -= Time.deltaTime;	
+					}
+					if (countdown <= 0) { 								                                  mak = false;						
+
+						countdown = 10.0f;
+					makrophage.SetActive (true);
+					}					
+				}
+
+		 // magnetisch ja/nein
+
+
+
+
+
+			}
+		}
+
+		
+
+
+
+	
