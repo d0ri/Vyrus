89cfc830ec76970f27930ec37b5b8e50b1e7aca4
Alex <alexander.freisinger@gmail.com>
2016-07-04 21:20:53 +0200

    Level 6 bearbeitet und FarbMischverhältnis geändert
    Zellen werden jetzt 6:1 statt 9:1 gemischt

--- Vyrus_Unity/Assets/Scripts/Farbe.cs
+++ Vyrus_Unity/Assets/Scripts/Farbe.cs
@@ -14,9 +14,9 @@ public class Farbe : MonoBehaviour {
 		if (other.transform.tag == "Zelle") {
 			old = GetComponent<Renderer> ().material.GetColor("_SpecColor") ; //aktuelle Farbe
 			zelle= other.GetComponent<Renderer> ().material.GetColor("_Color"); //Farbe der Zelle, ###!!!hier eventuell spaeter specColor verwenden falls Texturen verwendet werden###
-			r = (9*old.r+zelle.r)/10; 
-			g = (9*old.g+zelle.g)/10; //Mischt Wert im Verhältnis 9 zu 1
-			b = (9*old.b+zelle.b)/10; 
+			r = (6*old.r+zelle.r)/7; 
+			g = (6*old.g+zelle.g)/7; //Mischt Wert im Verhältnis 9 zu 1
+			b = (6*old.b+zelle.b)/7; 
 			GetComponent<Renderer> ().material.SetColor ("_SpecColor", new Color (r, g, b, 0.7f));//neue Farbe
 			GetComponent<Renderer> ().material.SetColor ("_EmissionColor", new Color (r, g, b, 0.7f));//neue Emmission-Farbe
 		}
