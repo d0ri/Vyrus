ae59486d96e738691d43606b5d69f38c2ddc7fab
Susan Martin <susi11189@aol.com>
2016-07-06 20:21:00 +0200

    Vorspann in Unity eingefügt

---
+++ Vyrus_Unity/Assets/Scripts/Vorspann.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;    // Weil Vorspann auf UI gezeigt wird
+
+[RequireComponent (typeof(AudioSource))]  //AudioQuelle als Voraussetzung, dass man sie nicht vergisst
+
+public class Vorspann : MonoBehaviour {
+
+	public MovieTexture movie;    //Film Textur, in die man den gewünschten Film ziehen kann
+	private AudioSource audio;     //AudioQuelle als private Komponente
+
+	void Start () {
+		GetComponent<RawImage>().texture = movie as MovieTexture; //dem RawImnage zeigen, dass die ausgewählte Textur als Film abgespielt werden soll
+		audio = GetComponent<AudioSource>();
+		audio.clip = movie.audioClip;        //das der Ton vom Film abgespielt wird
+		movie.Play();               //Film wird abgespielt
+		audio.Play ();              //Audio wird abgespielt
+	}
+
+
+
+	void Update () {
+
+		if(!movie.isPlaying) Application.LoadLevel("1_Johanna"); //wenn der Vorspann nicht läuft/beendet ist/stoppt, kommt man automatisch zu Level1 
+
+		if (Input.GetKeyDown (KeyCode.Space) && movie.isPlaying) {  //wenn Leertaste gedrückt wird, wenn Film läuft
+
+			movie.Stop();                                        //stoppt der Film
+		}
+
+
+	}
+}
