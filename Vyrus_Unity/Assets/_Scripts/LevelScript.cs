1deb2ce56e80bdb14e8fc06993ec3d05b53316d7
Alex <alexander.freisinger@gmail.com>
2016-07-10 20:17:20 +0200

    LevelScript erstellt
    +Bugfixes
    +assets aufgeräumt

---
+++ Vyrus_Unity/Assets/_Scripts/LevelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelScript : MonoBehaviour {
+
+	public GameObject[] Organe;
+	bool beendet = false;
+	public int LevelInt = 1;
+
+	void Update () {
+		for (int i = 0; i < Organe.Length; i++) {
+			if (!Organe[i].activeInHierarchy) {
+				beendet = true;
+			} else {
+				beendet = false;
+			}
+		}
+		if (beendet == true) {
+			Debug.Log("beendet");
+			SceneManager.LoadScene (LevelInt);
+		}
+	}	
+}
