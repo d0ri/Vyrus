16cb8574fca14a37a4698e9da8bb2eecb7ff6b44
Alex <alexander.freisinger@gmail.com>
2016-07-29 00:45:42 +0200

    Endanimation hinzugefügt und Bugfixes

--- Vyrus_Unity/Assets/_Scripts/6_Video&UI/Vorspann.cs
+++ Vyrus_Unity/Assets/_Scripts/6_Video&UI/Vorspann.cs
@@ -9,6 +9,7 @@ public class Vorspann : MonoBehaviour {
 
 	public MovieTexture movie;    //Film Textur, in die man den gewünschten Film ziehen kann
 	private AudioSource audio;     //AudioQuelle als private Komponente
+	public int lev = 1;
 
 	void Start () {
 		GetComponent<RawImage>().texture = movie as MovieTexture; //dem RawImnage zeigen, dass die ausgewählte Textur als Film abgespielt werden soll
@@ -22,12 +23,12 @@ public class Vorspann : MonoBehaviour {
 
 	void Update () {
 
-		if(!movie.isPlaying) Application.LoadLevel("1_Johanna"); //wenn der Vorspann nicht läuft/beendet ist/stoppt, kommt man automatisch zu Level1 
-		//if(!movie.isPlaying) SceneManager.LoadScene(1);   //statt Application.LoadLevel
+		//if(!movie.isPlaying) Application.LoadLevel("1_Johanna"); //wenn der Vorspann nicht läuft/beendet ist/stoppt, kommt man automatisch zu Level1 
+		if(!movie.isPlaying) SceneManager.LoadScene(lev);   //statt Application.LoadLevel
 
-		if (Input.GetKeyDown (KeyCode.Space) && movie.isPlaying) {  //wenn Leertaste gedrückt wird, wenn Film läuft
+		if (Input.GetKeyDown (KeyCode.Space)) {  //wenn Leertaste gedrückt wird, wenn Film läuft
 
-			movie.Stop();                                        //stoppt der Film
+			SceneManager.LoadScene(lev);                                      //läd nächsten level
 		}
 
 
